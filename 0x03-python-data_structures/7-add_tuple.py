c01b36e36270d785364cb5acd826c5db74756dfd
kanza maadan <kenza maadan@gmail.com>
2024-03-20 10:24:42 -0700

    7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,5 +8,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_b += (0, 0)
 
     # Return a tuple containing the sums of
-    #corresponding elements of tuple_a and tuple_b
+    # corresponding elements of tuple_a and tuple_b
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
