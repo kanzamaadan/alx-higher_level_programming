4c0ec697f93b5785ce05dda77070dc2b9cf3b89e
kanza maadan <kenza maadan@gmail.com>
2024-03-22 23:37:15 +0000

    9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    my_dictionary = {}
+    for key, value in a_dictionary.items():
+        my_dictionary[key] = value * 2
+    return my_dictionary
