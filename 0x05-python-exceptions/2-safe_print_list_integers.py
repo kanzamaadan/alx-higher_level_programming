ef058947352cd8d6a48a3b716c6a025375f58b08
kanza maadan <kenza maadan@gmail.com>
2024-04-03 11:16:34 -0700

    last upddate

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,12 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     cnt = 0
-    try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                cnt += 1
-    except (ValueError, TypeError):
-        pass
-    finally:
-        print()
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (ValueError, TypeError):
+            pass
+        else:
+            cnt += 1
+    print()
     return cnt
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,15 +5,15 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             my_result = my_list_1[i] / my_list_2[i]
-        except IndexError:
-            print("out of range")
-            result = 0
-        except TypeError:
+        except (TypeError):
             print("wrong type")
-            result = 0
-        except ZeroDivisionError:
+            my_result = 0
+        except (IndexError):
+            print("out of range")
+            my_result = 0
+        except (ZeroDivisionError):
             print("division by 0")
-            result = 0
+            my_result = 0
         finally:
             my_list.append(my_result)
-    return my_list
+    return (my_list)
