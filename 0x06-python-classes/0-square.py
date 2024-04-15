bc1f5f3fdf09bfbab738ea6bd51d1194278379d9
kanza maadan <kenza maadan@gmail.com>
2024-04-14 17:12:49 -0700

    last update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+""" Working on oop with python """
+
 
 class Square():
-    def Square(self):
-        pass
+    """Class representing a square"""
+
+    def __init__(self, size):
+        """ initialize the class
+
+        Args:
+           size (int): size of the square
+        """
+        self.__size = size
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
-class Square():
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
 
     def __init__(self, size):
+        """Initialize the Square class with a given size."""
         self.__size = size
