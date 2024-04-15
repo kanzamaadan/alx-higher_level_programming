9cee2d5c96bdae2f56d18f2b2700b659df041449
kanza maadan <kenza maadan@gmail.com>
2024-04-15 08:32:29 -0700

    6

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""function that multiplies 2 matrices by using NumPy"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """function that multiplies 2 matrices by using NumPy
+    Args:
+        @m_a: the 1st matrix to multiply
+        @m_b: the 1st matrix to multiply
+    Returns:
+        Return the multiplication of two matrices
+    """
+    return (np.matmult(m_a, m_b))
