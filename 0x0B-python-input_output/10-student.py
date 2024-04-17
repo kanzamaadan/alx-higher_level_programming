4570c4cb7365386815edf62d4fa157410892c50a
kanza maadan <kenza maadan@gmail.com>
2024-04-16 18:56:10 -0700

    10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""class Student that defines a studen"""
+
+
+class Student():
+    """class Student that defines a studen"""
+    def __init__(self, first_name, last_name, age):
+        """initializes a Student.
+        Args:
+            first_name: string for the first name of student
+            last_name: string for the last name of student
+            age: int for the age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if (type(attrs) is list and
+                all(type(ele) is str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
