3c740d14bce765fed96d07ca0cacec0da2b0538c
kanza maadan <kenza maadan@gmail.com>
2024-04-17 10:53:09 -0700

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Defines class BaseGeometry."""
+
+
+class BaseGeometry():
+    """this is an empty class BaseGeometry"""
+
+    def area(self):
+        """raises an Exception with the message"""
+        raise Exception("area() is not implemented")
