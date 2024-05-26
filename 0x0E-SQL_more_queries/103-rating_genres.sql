054ac8b200a4245d2c38b7220bea4fba8a850d5c
kanza maadan <kenza maadan@gmail.com>
2024-05-26 11:11:49 -0700

    last update

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,6 +3,6 @@ SELECT tv_genres.name, SUM(rate) AS rating
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 INNER JOIN tv_show_ratings
-ON tv_show_ratings.show_id = tv_show_ratings.show_id
+ON tv_show_ratings.show_id = tv_show_genres.show_id
 GROUP BY tv_genres.name
 ORDER BY rating DESC;
